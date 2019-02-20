242c1b85a0ae404a6517644b32dd60a3f699502f
NemoFlying <895190626@qq.com>
2019-02-21 00:38:56 +0800

    123

--- TakeOut/BLL/Role/Dto/RoleInfoInput.cs
+++ TakeOut/BLL/Role/Dto/RoleInfoInput.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
-namespace TakeOut.BLL
+namespace TakeOut.BLL.Role.Dto
 {
-    public class BseService
+    public class RoleInfoInput
     {
     }
 }
