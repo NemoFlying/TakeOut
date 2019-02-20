0dc3062f78625b03a52acffcf66ef85dd17b4123
NemoFlying <895190626@qq.com>
2019-02-20 23:14:11 +0800

    UserService编写

--- TakeOut/BLL/BaseService.cs
+++ TakeOut/BLL/BaseService.cs
@@ -8,11 +8,32 @@ namespace TakeOut.BLL
 {
     public abstract partial class BaseService<T> where T : class, new()
     {
-        public IBaseDAL<T> Dal { get; set; }
+        public IBaseDAL<T> _dAL { get; set; }
 
         //public bool Add(T entity)
         //{
         //    Dal
         //}
+
+        //public bool DeleteModlesByIds(List<int> Ids)
+        //{
+        //    Ids.ForEach(id =>
+        //    {
+        //        T.Delete(
+        //            _userDAL.GetModels(con => con.Id == id)
+        //            .FirstOrDefault()
+        //            );
+        //    });
+        //    try
+        //    {
+        //        return _userDAL.SaveChanges();
+        //    }
+        //    catch
+        //    {
+        //        //日志记录
+        //        return false;
+        //    }
+        //}
+
     }
 }
