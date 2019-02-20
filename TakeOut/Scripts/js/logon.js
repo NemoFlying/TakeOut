e0ffd5733d5dfed28d9793f4759a9a5843227a8e
luomingsong <240269961@qq.com>
2019-02-20 21:37:57 +0800

    新增前端页面

---
+++ TakeOut/Scripts/js/logon.js
@@ -0,0 +1,13 @@
+﻿
+window.onload = function () {
+    var bodyH = $(window).height();
+    $("body").css({ "height": bodyH + "px" });
+    $(".regBtn").on("click", function () {
+        $(".login").hide();
+        $(".reg").show();
+    });
+    $(".loginBtn").on("click", function () {
+        $(".login").show();
+        $(".reg").hide();
+    });
+}
